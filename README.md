d8eff2edc69ab6d91df8ae031cb570b4b11936a2
Trm888 <bigtram888@gmail.com>
2022-11-07 17:21:16 +0900

    Final script

---
+++ README.md
@@ -0,0 +1,25 @@
+#  Comics publisher
+****
+### Description
+Script for publishing comics in a group in VK
+
+### Installation and launch
+- Install dependencies from requirements.txt file.
+```
+pip install -r requirements.txt
+```
+- Get a token from this link https://vk.com/dev/implicit_flow_user.
+-  Add the ".env" file to the project folder and enter the following data:
+```
+GROUP_ID="id of your group in vk."
+ACCESS_TOKEN="Your token"
+
+```
+-  script launch:
+```
+python script_comics_publisher.py
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ script_comics_publisher.py
@@ -0,0 +1,124 @@
+import os
+import random
+from pathlib import Path
+from urllib.parse import urlparse, unquote
+
+import requests
+from environs import Env
+
+
+def get_number_comics():
+    api_url = 'https://xkcd.com/info.0.json'
+    response = requests.get(api_url)
+    response.raise_for_status()
+    return response.json()['num']
+
+
+def extract_extension(url):
+    url_path = urlparse(url).path
+    return os.path.splitext(unquote(url_path, encoding='utf-8',
+                                    errors='replace'))[1]
+
+
+def get_random_id():
+    comics_id = random.randint(1, get_number_comics())
+    return comics_id
+
+
+def get_message_img(comics_id):
+    comics_url = f'https://xkcd.com/{comics_id}/info.0.json'
+    response = requests.get(comics_url)
+    response.raise_for_status()
+    decoded_response = response.json()
+    return decoded_response['alt']
+
+
+def remove_images():
+    path = Path(os.getcwd(), 'image')
+    for image in os.listdir(path):
+        os.remove(Path(path, image))
+
+
+def download_image(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
+def download_random_image(comics_id):
+    comics_url = f'https://xkcd.com/{comics_id}/info.0.json'
+    response = requests.get(comics_url)
+    response.raise_for_status()
+    decoded_response = response.json()
+    extension = extract_extension(decoded_response['img'])
+    Path(os.getcwd(), 'image').mkdir(parents=True, exist_ok=True)
+    filepath = Path(os.getcwd(), 'image',
+                    f'python_comics_{comics_id}{extension}')
+    download_image(decoded_response['img'], filepath)
+    return filepath
+
+
+def get_upload_server(group_id, token, api_version):
+    api_url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {'group_id': group_id,
+              'access_token': token,
+              'v': api_version}
+    response = requests.get(api_url, params=params)
+    wall_upload_server_info = response.json()
+    return wall_upload_server_info['response']['upload_url']
+
+
+def send_and_save_photo(comics_id, group_id, token, api_version):
+    filepath = download_random_image(comics_id)
+    with open(filepath, 'rb') as file:
+        url = get_upload_server(group_id, token, api_version)
+        files = {
+            'photo': file,
+        }
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+    params_from_server = response.json()
+    api_url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    params = {'group_id': group_id,
+              'access_token': token,
+              'photo': params_from_server['photo'],
+              'server': params_from_server['server'],
+              'hash': params_from_server['hash'],
+              'v': api_version}
+    response = requests.post(api_url, params=params)
+    response.raise_for_status()
+    params_from_wall = response.json()
+    return params_from_wall
+
+
+def post_comics(comics_id, group_id, token, api_version):
+    api_url = 'https://api.vk.com/method/wall.post'
+    params_from_wall = send_and_save_photo(comics_id, group_id,
+                                           token, api_version)
+    message = get_message_img(comics_id)
+    attachments = f'photo{params_from_wall["response"][0]["owner_id"]}_' \
+                  f'{params_from_wall["response"][0]["id"]}'
+    params = {'owner_id': f'-{group_id}',
+              'access_token': token,
+              'from_group': 1,
+              'attachments': attachments,
+              'message': message,
+              'v': api_version}
+    response = requests.post(api_url, params=params)
+    response.raise_for_status()
+
+
+def main():
+    comics_id = get_random_id()
+    env = Env()
+    env.read_env()
+    token = env.str("ACCESS_TOKEN")
+    group_id = env.str("GROUP_ID")
+    api_version = 5.131
+    post_comics(comics_id, group_id, token, api_version)
+    remove_images()
+
+
+if __name__ == '__main__':
+    main()
